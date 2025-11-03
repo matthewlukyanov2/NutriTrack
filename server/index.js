17e853a71bde059071a64c7fcd82ba4259251de2
matthewlukyanov2 <brucelukyanov@gmail.com>
2025-11-03 22:48:46 +0000

    Added security middleware (cors, helmet) and initialized my route structure

--- server/index.js
+++ server/index.js
@@ -2,9 +2,20 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const cors = require("cors");
+const helmet = require("helmet");
+
+// Import routes 
+const authRoutes = require("./routes/authRoutes");
+const mealRoutes = require("./routes/mealRoutes");
+const workoutRoutes = require("./routes/workoutRoutes");
+const aiRoutes = require("./routes/aiRoutes");
+
 
 // Middleware
 app.use(express.json());
+app.use(cors());
+app.use(helmet());
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
@@ -15,6 +26,12 @@ mongoose.connect(process.env.MONGO_URI)
 app.get("/", (req, res) => {
   res.send("NutriTrack API is running...");
 });
+
+// API routes
+app.use("/api/auth", authRoutes);
+app.use("/api/meals", mealRoutes);
+app.use("/api/workouts", workoutRoutes);
+app.use("/api/ai", aiRoutes);
 
 // Start server
 const PORT = process.env.PORT || 5000;
