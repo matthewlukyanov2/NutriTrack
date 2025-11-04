6a43f37e2b157399682d1d826b4fe6de182183d9
matthewlukyanov2 <brucelukyanov@gmail.com>
2025-11-04 17:17:15 +0000

    Added basic AI route placeholder for testing

---
+++ server/routes/aiRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+// Temporary test route
+router.get("/", (req, res) => {
+  res.send("AI route working!");
+});
+
+module.exports = router;
