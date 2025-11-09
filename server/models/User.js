5485ba5a5aef9d6d4b27e760fbadb4b381af3fbd
matthewlukyanov2 <brucelukyanov@gmail.com>
2025-11-09 20:12:37 +0000

    Added password comparison method to User model for login validation

--- server/models/User.js
+++ server/models/User.js
@@ -27,5 +27,10 @@ userSchema.pre('save', async function (next) {
   next();
 });
 
+// Compare password for login
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+  };
+  
 
 module.exports = mongoose.model('User', userSchema);
