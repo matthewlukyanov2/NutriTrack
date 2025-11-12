bcbf9f35fda05661a5a8290dbf8f86d59cfe1cb9
matthewlukyanov2 <brucelukyanov@gmail.com>
2025-11-12 21:26:35 +0000

    Added basic meal model schema for storing user meal data

---
+++ server/models/Meal.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const mealSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    calories: {
+      type: Number,
+      required: true,
+    },
+    protein: {
+      type: Number,
+      default: 0,
+    },
+    carbs: {
+      type: Number,
+      default: 0,
+    },
+    fats: {
+      type: Number,
+      default: 0,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Meal', mealSchema);
