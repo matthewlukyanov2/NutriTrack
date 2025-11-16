be0d4097096e9352271e3e744db00bba92df7d0b
matthewlukyanov2 <brucelukyanov@gmail.com>
2025-11-16 22:20:11 +0000

    Added JWT extraction and no-token response

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -3,5 +3,13 @@ const User = require('../models/User');
 
 // Protect routes
 exports.protect = async (req, res, next) => {
+    let token;
 
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+      token = req.headers.authorization.split(' ')[1];
+    }
+  
+    if (!token) {
+      return res.status(401).json({ message: 'Not authorized, no token' });
+    }
 };
