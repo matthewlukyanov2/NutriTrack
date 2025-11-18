443072634bc3b142b9057b27e56a64f438ede52e
matthewlukyanov2 <brucelukyanov@gmail.com>
2025-11-18 21:14:38 +0000

    Added JWT verification logic

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -12,4 +12,14 @@ exports.protect = async (req, res, next) => {
     if (!token) {
       return res.status(401).json({ message: 'Not authorized, no token' });
     }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = await User.findById(decoded.id).select('-password');
+        next();
+      } catch (error) {
+        console.error(error);
+        return res.status(401).json({ message: 'Not authorized, token failed' });
+      }
+      
 };
