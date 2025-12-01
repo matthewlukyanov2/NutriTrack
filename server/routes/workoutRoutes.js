765585e34052fd70310b563a5ec0e36a90cc3766
matthewlukyanov2 <brucelukyanov@gmail.com>
2025-12-01 20:23:57 +0000

    Added workout routes and applied auth middleware2

--- server/routes/workoutRoutes.js
+++ server/routes/workoutRoutes.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
-// Temporary test route
-router.get("/", (req, res) => {
-  res.send("Workout route working!");
-});
+const {
+  addWorkout,
+  getWorkouts,
+  getWorkoutById,
+  updateWorkout,
+  deleteWorkout
+} = require('../controllers/workoutController');
+
+const protect = require('../middleware/authMiddleware');
+
+// Protected routes
+router.post('/', protect, addWorkout);
+router.get('/', protect, getWorkouts);
+router.get('/:id', protect, getWorkoutById);
+router.put('/:id', protect, updateWorkout);
+router.delete('/:id', protect, deleteWorkout);
 
 module.exports = router;
