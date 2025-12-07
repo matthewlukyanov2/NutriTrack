4a3db92bc9d403c95a45bf61e6e89d7816f246e2
matthewlukyanov2 <brucelukyanov@gmail.com>
2025-12-07 00:18:31 +0000

    Fixed user.js error

--- server/models/User.js
+++ server/models/User.js
@@ -19,6 +19,7 @@ const userSchema = new mongoose.Schema({
   },
 }, { timestamps: true });
 
+
 // Hash password before saving
 userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
