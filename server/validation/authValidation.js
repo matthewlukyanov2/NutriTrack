e526063bc598279104faa27a976bb9068fcc161a
matthewlukyanov2 <brucelukyanov@gmail.com>
2025-12-30 21:45:56 +0000

    Add Joi validation schemas for auth

---
+++ server/validation/authValidation.js
@@ -0,0 +1,19 @@
+const Joi = require('joi');
+
+// Register validation
+const registerSchema = Joi.object({
+  name: Joi.string().min(2).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
+});
+
+// Login validation
+const loginSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+});
+
+module.exports = {
+  registerSchema,
+  loginSchema,
+};
