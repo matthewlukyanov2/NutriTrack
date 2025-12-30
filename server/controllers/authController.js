1a3b4c051bf2a0c3a6260a5975a71ae0196ea81f
matthewlukyanov2 <brucelukyanov@gmail.com>
2025-12-30 22:41:42 +0000

    Added Joi validation to loginUser

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -40,6 +40,11 @@ exports.registerUser = async (req, res) => {
 // Login user
 exports.loginUser = async (req, res) => {
   try {
+    const { error } = loginSchema.validate(req.body);
+
+    if (error) {
+      return res.status(400).json({ message: error.details[0].message });
+    }
     const { email, password } = req.body;
 
     const user = await User.findOne({ email });
