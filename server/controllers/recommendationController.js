66eff76dcc25bd110a715d564ea5dba1c5d6855f
matthewlukyanov2 <brucelukyanov@gmail.com>
2026-01-03 22:11:23 +0000

    Added getmealrec function for future tf idf feature

---
+++ server/controllers/recommendationController.js
@@ -0,0 +1,21 @@
+const Meal = require('../models/Meal');
+const tfidfService = require('../services/tfidfService');
+
+// @desc    Get meal recommendations (future TF-IDF)
+// @route   GET /api/recommendations/meals
+// @access  Private
+exports.getMealRecommendations = async (req, res) => {
+  try {
+    const userMeals = await Meal.find({ user: req.user.id });
+    const allMeals = await Meal.find();
+
+    const recommendations = tfidfService.recommend(userMeals, allMeals);
+
+    res.status(200).json({
+      message: 'Recommendation engine scaffolded',
+      recommendations,
+    });
+  } catch (error) {
+    res.status(400).json({ message: 'Failed to get recommendations' });
+  }
+};
