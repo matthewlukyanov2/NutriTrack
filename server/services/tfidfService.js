5cd494d78c54d4105818dab7930e0e3b0862e380
matthewlukyanov2 <brucelukyanov@gmail.com>
2026-01-05 21:51:34 +0000

    Added score meals function to measure score

--- server/services/tfidfService.js
+++ server/services/tfidfService.js
@@ -2,6 +2,7 @@ const natural = require('natural');
 const TfIdf = natural.TfIdf;
 
 class TfidfService {
+    recommendMeals(meals, limit = 3) {
     if (!meals || meals.length === 0) return [];
 
     const tfidf = new TfIdf();
@@ -16,6 +17,23 @@ class TfidfService {
       `;
       tfidf.addDocument(doc);
     });
+
+    // 2. Score meals
+    const scoredMeals = meals.map((meal, index) => {
+        let score = 0;
+  
+        tfidf.tfidfs(meal.name, (i, measure) => {
+          if (i === index) score = measure;
+        });
+  
+        return {
+          meal,
+          score
+        };
+      });
+ 
+}
+      
 
   }
   
