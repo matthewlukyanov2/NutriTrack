0bf32508863782d6aadae79dc1128ae8db87fe58
matthewlukyanov2 <brucelukyanov@gmail.com>
2026-01-07 23:04:11 +0000

    Added swagger js content to show options and components

--- server/config/swagger.js
+++ server/config/swagger.js
@@ -1,1 +1,30 @@
 const swaggerJsdoc = require('swagger-jsdoc');
+
+const options = {
+  definition: {
+    openapi: '',
+    info: {
+      title: '',
+      version: '',
+      description: ''
+    },
+    servers: [
+      {
+        url: ''
+      }
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT'
+        }
+      }
+    },
+    security: [{ bearerAuth: [] }]
+  },
+  apis: []
+};
+
+module.exports = swaggerJsdoc(options);
