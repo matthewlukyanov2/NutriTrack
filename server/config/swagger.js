b0aeb7e8bf44c03ecae8bbd9305af63d7642e08f
matthewlukyanov2 <brucelukyanov@gmail.com>
2026-01-08 21:56:43 +0000

    Changed swagger js file to include schema in the backend web page host

--- server/config/swagger.js
+++ server/config/swagger.js
@@ -6,25 +6,52 @@ const options = {
     info: {
       title: 'NutriTrack API',
       version: '1.0.0',
-      description: 'Backend API for NutriTrack fitness and nutrition app'
+      description: 'Backend API for NutriTrack fitness and nutrition app',
     },
     servers: [
       {
-        url: 'http://localhost:5000'
-      }
+        url: 'http://localhost:5000',
+      },
     ],
     components: {
       securitySchemes: {
         bearerAuth: {
           type: 'http',
           scheme: 'bearer',
-          bearerFormat: 'JWT'
-        }
-      }
+          bearerFormat: 'JWT',
+        },
+      },
+      schemas: {
+        Meal: {
+          type: 'object',
+          required: ['name', 'calories', 'protein', 'carbs', 'fats'],
+          properties: {
+            name: {
+              type: 'string',
+              example: 'Chicken & Rice',
+            },
+            calories: {
+              type: 'number',
+              example: 550,
+            },
+            protein: {
+              type: 'number',
+              example: 45,
+            },
+            carbs: {
+              type: 'number',
+              example: 60,
+            },
+            fats: {
+              type: 'number',
+              example: 12,
+            },
+          },
+        },
+      },
     },
-    security: [{ bearerAuth: [] }]
   },
-  apis: ['./routes/*.js']
+  apis: ['./routes/*.js'],
 };
 
 module.exports = swaggerJsdoc(options);
