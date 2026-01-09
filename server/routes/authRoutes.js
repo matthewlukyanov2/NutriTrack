6d4e78e585649819cebf1eda050074a66175f489
matthewlukyanov2 <brucelukyanov@gmail.com>
2026-01-09 23:34:25 +0000

    Added swagger documentation for authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -4,7 +4,50 @@ const router = express.Router();
 const validate = require('../middleware/validateMiddleware');
 const { registerSchema, loginSchema } = require('../validation/authValidation');
 
+/**
+ * @swagger
+ * /api/auth/register:
+ *   post:
+ *     summary: Register a new user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/UserRegister'
+ *     responses:
+ *       201:
+ *         description: User registered successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/AuthResponse'
+ */
+
 router.post('/register', validate(registerSchema), registerUser);
+
+/**
+ * @swagger
+ * /api/auth/login:
+ *   post:
+ *     summary: Login user and return JWT
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/UserLogin'
+ *     responses:
+ *       200:
+ *         description: Login successful
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/AuthResponse'
+ */
+
 router.post('/login', validate(loginSchema), loginUser);
 
 module.exports = router;
