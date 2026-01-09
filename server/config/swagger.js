761b7e88c67ed8e4d2b89413f9ef891688358d23
matthewlukyanov2 <brucelukyanov@gmail.com>
2026-01-09 23:28:46 +0000

    Added UserRegister, UserLogin and AuthResponse to my schemas in swagger.js file

--- server/config/swagger.js
+++ server/config/swagger.js
@@ -26,31 +26,57 @@ const options = {
           type: 'object',
           required: ['name', 'calories', 'protein', 'carbs', 'fats'],
           properties: {
-            name: {
-              type: 'string',
-              example: 'Chicken & Rice',
-            },
-            calories: {
-              type: 'number',
-              example: 550,
-            },
-            protein: {
-              type: 'number',
-              example: 45,
-            },
-            carbs: {
-              type: 'number',
-              example: 60,
-            },
-            fats: {
-              type: 'number',
-              example: 12,
-            },
-          },
-        },
-      },
+            name: { type: 'string', example: 'Chicken & Rice' },
+      calories: { type: 'number', example: 550 },
+      protein: { type: 'number', example: 45 },
+      carbs: { type: 'number', example: 60 },
+      fats: { type: 'number', example: 12 },
     },
   },
+
+  Workout: {
+    type: 'object',
+    required: ['type', 'duration', 'caloriesBurned'],
+    properties: {
+      type: { type: 'string', example: 'Running' },
+      duration: { type: 'number', example: 45 },
+      caloriesBurned: { type: 'number', example: 300 },
+    },
+  },
+
+  UserRegister: {
+    type: 'object',
+    required: ['name', 'email', 'password'],
+    properties: {
+      name: { type: 'string', example: 'Test User' },
+      email: { type: 'string', example: 'test@example.com' },
+      password: { type: 'string', example: '123456' },
+    },
+  },
+
+  UserLogin: {
+    type: 'object',
+    required: ['email', 'password'],
+    properties: {
+      email: { type: 'string', example: 'test@example.com' },
+      password: { type: 'string', example: '123456' },
+    },
+  },
+
+  AuthResponse: {
+    type: 'object',
+    properties: {
+      _id: { type: 'string' },
+      name: { type: 'string' },
+      email: { type: 'string' },
+      token: { type: 'string' },
+    },
+  },
+},
+            
+      },
+    },
+ 
   apis: ['./routes/*.js'],
 };
 
