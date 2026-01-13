98b106e43c9a68987ed5e7c3b2957c0b1b678201
matthewlukyanov2 <brucelukyanov@gmail.com>
2026-01-13 19:46:37 +0000

    Add auth token to API requests

--- server/client/src/services/api.js
+++ server/client/src/services/api.js
@@ -4,6 +4,13 @@ const API = axios.create({
   baseURL: "http://localhost:5000/api",
 });
 
-
+// Attach token if exists
+API.interceptors.request.use((req) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
+  return req;
+});
 
 export default API;
