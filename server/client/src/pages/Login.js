e175224668f4926834fd63438ad3bb764ed2344d
matthewlukyanov2 <brucelukyanov@gmail.com>
2026-01-13 20:47:20 +0000

    Connected login form to authentication API

--- server/client/src/pages/Login.js
+++ server/client/src/pages/Login.js
@@ -1,11 +1,22 @@
 import { useState } from "react";
+import API from "../services/api";
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleLogin = (e) => {
+  const handleLogin = async (e) => {
     e.preventDefault();
+    try {
+        const res = await API.post("/auth/login", {
+          email,
+          password,
+        });
+        localStorage.setItem("token", res.data.token);
+        alert("Login successful!");
+      } catch (err) {
+        alert("Login failed");
+      }
   };
 
   return (
