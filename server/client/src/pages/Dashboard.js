62c9ec5caa586131f01acefe91bb01cadb57bb84
matthewlukyanov2 <brucelukyanov@gmail.com>
2026-02-15 21:36:55 +0000

    Attempt to debug ResponsiveContainer console error

--- server/client/src/pages/Dashboard.js
+++ server/client/src/pages/Dashboard.js
@@ -411,7 +411,7 @@ const chartData = meals.map((meal) => ({
 </p>
 
 <div style={{ width: "100%", height: 250 }}>
-  <ResponsiveContainer>
+  <ResponsiveContainer width="100%" height="100%">
     <BarChart data={chartData}>
       <XAxis dataKey="name" />
       <YAxis />
