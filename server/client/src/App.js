6277550948f1d652649db37472ec441b35cb30a0
matthewlukyanov2 <brucelukyanov@gmail.com>
2026-03-28 23:45:56 +0000

    Added appropriate route for register in app.js

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -1,4 +1,5 @@
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import ProtectedRoute from "./components/ProtectedRoute";
@@ -8,6 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route
           path="/"
           element={
