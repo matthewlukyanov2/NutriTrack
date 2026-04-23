62cd7a89fed59dea8e22d5248ea1012c01f6214a
matthewlukyanov2 <brucelukyanov@gmail.com>
2026-04-23 23:44:09 +0100

    et up multi-page structure with routing and shared TopNav component

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -2,6 +2,11 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
+import MealsPage from "./pages/MealsPage";
+import WorkoutsPage from "./pages/WorkoutsPage";
+import RecommendationsPage from "./pages/RecommendationsPage";
+import MealPlannerPage from "./pages/MealPlannerPage";
+import GoalsPage from "./pages/GoalsPage";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
@@ -15,6 +20,51 @@ function App() {
           element={
             <ProtectedRoute>
               <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/meals"
+          element={
+            <ProtectedRoute>
+              <MealsPage />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/workouts"
+          element={
+            <ProtectedRoute>
+              <WorkoutsPage />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/recommendations"
+          element={
+            <ProtectedRoute>
+              <RecommendationsPage />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/meal-planner"
+          element={
+            <ProtectedRoute>
+              <MealPlannerPage />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/goals"
+          element={
+            <ProtectedRoute>
+              <GoalsPage />
             </ProtectedRoute>
           }
         />
